4058919aae48c3be43fcb9a3f850a3ddb88f4c47
Jeetesh <jeeteshkumar45@gmail.com>
2023-08-12 22:49:30 +0530

    Car-card with carAPI

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,13 +1,29 @@
-"use client"
-import { CustomButtonProps } from "@/types"
-import Image from "next/image"
-const CustomButton = ({title,containerStyles,handleClick,btnType}:CustomButtonProps) => {
+"use client";
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  rightIcon,
+  textStyles,
+}: CustomButtonProps) => {
   return (
-    <button className={`custom-btn ${containerStyles}`} disabled={false} type={btnType||'button'} onClick={handleClick}>
-        <span className={`flex-1`}>{title}</span>
-     
+    <button
+      className={`custom-btn ${containerStyles}`}
+      disabled={false}
+      type={btnType || "button"}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} fill className="object-contain" alt="icon" />
+        </div>
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
